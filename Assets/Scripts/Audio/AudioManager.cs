4547534976cf6f1501b656299f50d9b0c3d45750
antoonrs <arodriguezseselle@gmail.com>
2024-05-09 21:24:37 +0200

    FMOD implementation

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,47 +1,62 @@
 using System;
 using UnityEngine;
-
+using FMODUnity;  // Importante para interactuar con FMOD
 
 public class AudioManager : MonoBehaviour, IAudioService
 {
+    private FMOD.Studio.EventInstance gameplayMusic; // Instancia para manejar la música de juego
+    private FMOD.Studio.Bus masterBus; // Para controlar el bus maestro, por ejemplo para pausas
+
     private void Start()
     {
-        GameManager.Instance.Get<IPeopleService>().OnPeopleChanged.AddListener(OnPeopleChanged);
+        //GameManager.Instance.Get<IPeopleService>().OnPeopleChanged.AddListener(OnPeopleChanged);
+
+        masterBus = RuntimeManager.GetBus("bus:/"); // Asegúrate de poner la ruta correcta del bus en FMOD
+        StartGameplayMusic();
+
+        //PlaySound("MUSIC/Credits");
     }
 
     private void OnDestroy()
     {
-        GameManager.Instance.Get<IPeopleService>().OnPeopleChanged.RemoveListener(OnPeopleChanged);
+        //GameManager.Instance.Get<IPeopleService>().OnPeopleChanged.RemoveListener(OnPeopleChanged);
+        gameplayMusic.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        gameplayMusic.release();
     }
 
     public void PlaySound(string key)
     {
-        //implementala anton
+        var sound = RuntimeManager.CreateInstance("event:/" + key); // Asegúrate que las rutas coincidan con tus eventos en FMOD
+        sound.start();
+        sound.release();
     }
-    
+
     public void StartGameplayMusic()
     {
-            
+        gameplayMusic = RuntimeManager.CreateInstance("event:/MUSIC/Level");
+        gameplayMusic.start();
     }
-    
-    public void ChangeMusic(string key) //para cambiar de cancion (en la cinematica final etc)
+
+    public void ChangeMusic(string key)
     {
-            
+        gameplayMusic.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        gameplayMusic.release();
+        gameplayMusic = RuntimeManager.CreateInstance("event:/" + key);
+        gameplayMusic.start();
     }
-    
+
     public void StopMusic()
     {
-            
+        gameplayMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
-    
+
     public void ResumeMusic()
     {
-            
+        gameplayMusic.start();
     }
-    
-    
+
     private void OnPeopleChanged(uint people)
     {
-        //se llama cada vez que cambia la gente, aqui cambiarian los instrumentos y tal segun la gente que haya
+        gameplayMusic.setParameterByName("PeopleCount", people);
     }
 }
